a6357ccffb84f087c9c4b25e5c730633d1a9cfe6
Tural Kazimli <tural.kazimli00@mail.ru>
2022-05-02 22:45:41 +0200

    Add evaluation procedures for expression

---
+++ src/procedure.cpp
@@ -0,0 +1,35 @@
+#include "../include/procedure.hpp"
+
+Procedure::Procedure() = default;
+Procedure::~Procedure() = default;
+
+Env Procedure::get_proc_arg_list() const{
+    return proc_arg_list;
+}
+
+Env Procedure::get_proc_body() const{
+    return proc_body;
+}
+
+Procedure::Procedure(Env &expr) {
+    proc_arg_list = expr.get_member(1);
+    std::vector<std::string> temp;
+    
+    temp.push_back("(");
+    temp.push_back("begin");
+    
+    for (int i = 2; i < expr.get_num_exprs(); i++) {
+        add_env(temp, expr.get_member(i));
+    }
+
+    temp.push_back(")");
+
+    proc_body = Env(temp);
+}
+
+void Procedure::add_env(std::vector<std::string>&expr, Env env) const {
+    std::vector<std::string> temp = env.get_env();
+    for (int i = 0; i < temp.size(); i++) {
+        expr.push_back(temp[i]);
+    }
+}
