91d9c3ffff980752a2ef4fd3d1915257a77c0cb6
Tural Kazimli <tural.kazimli00@mail.ru>
2022-05-04 20:11:16 +0200

    Update arg names

--- include/operations.hpp
+++ include/operations.hpp
@@ -10,18 +10,18 @@
 #include <cstdlib>
 
 std::string print(std::vector<std::string> &expr);
-std::string add(std::vector<std::string>&vv);
-std::string subtract(std::vector<std::string>&vv);
-std::string divide(std::vector<std::string>&vv);
-std::string multiply(std::vector<std::string>&vv);
+std::string add(std::vector<std::string> &expr);
+std::string subtract(std::vector<std::string> &expr);
+std::string divide(std::vector<std::string> &expr);
+std::string multiply(std::vector<std::string> &expr);
 
-std::string equal(std::vector<std::string>&vv);
-std::string not_equal(std::vector<std::string>&vv);
-std::string less_than(std::vector<std::string>&vv);
-std::string less_than_or_equal(std::vector<std::string>&vv);
-std::string greater_than(std::vector<std::string>&vv);
-std::string greater_than_or_equal(std::vector<std::string>&vv);
+std::string equal(std::vector<std::string> &expr);
+std::string not_equal(std::vector<std::string> &expr);
+std::string less_than(std::vector<std::string> &expr);
+std::string less_than_or_equal(std::vector<std::string> &expr);
+std::string greater_than(std::vector<std::string> &expr);
+std::string greater_than_or_equal(std::vector<std::string> &expr);
 
-std::string scheme_quit(std::vector<std::string>&vv);
+std::string scheme_quit(std::vector<std::string> &expr);
 
 #endif
