e7679d49c46411217d5ffa7c51421b9b2691d9dd
Tural Kazimli <tural.kazimli00@mail.ru>
2022-05-10 16:09:00 +0200

    Add utility tests

---
+++ tests/utils_test.cpp
@@ -0,0 +1,36 @@
+#include "../include/utils.hpp"
+#include <gtest/gtest.h>
+
+namespace {
+    TEST(UtilsTest, CheckNumber) {
+        std::string sample = "+ 2 4";
+        auto expected = true;
+        EXPECT_EQ(expected, number(sample));
+
+        sample = "x";
+        expected = false;
+        EXPECT_EQ(expected, number(sample));
+
+        sample = "#t";
+        EXPECT_EQ(expected, number(sample));
+    }
+
+    TEST(UtilsTest, ParseInput) {
+        std::string sample = "(\n+\n\n\n2\n(\n*\n3\n4\n)\n)";
+        std::vector<std::string> expected =  {"(", "+", "2", "(", "*", "3", "4", ")", ")"};
+        EXPECT_EQ(expected, parse_input(sample));
+
+        sample = "(define\n\n(\nsquare\nx\n)\n(+\nx\n)\n\n)\n";
+        expected = {"(", "define", "(", "square", "x", ")", "(", "+", "x", ")", ")"};
+        EXPECT_EQ(expected, parse_input(sample));
+
+        sample = "(\n(\nlambda\n ( x )  \n( + \nx x ) \n) \n4\n)  ";
+        expected = {"(", "(", "lambda", "(", "x", ")", "(", "+", "x", "x", ")", ")", "4", ")"};
+        EXPECT_EQ(expected, parse_input(sample));
+    }
+
+    int main(int argc, char** argv) {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    }
+} 
